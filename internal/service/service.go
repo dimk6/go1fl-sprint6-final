e08e0f7d79ad08bdcd7472e98c1f362fb5020de2
dimk6 <dimon978@bk.ru>
2026-06-06 21:54:21 +0300

    Добавлен сервер и конвертер Морзе

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,30 @@
 package service
+
+import (
+	"errors"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func Convert(data string) (string, error) {
+	data = strings.TrimSpace(data)
+	if data == "" {
+		return "", errors.New("empty file")
+	}
+
+	if isMorse(data) {
+		return morse.ToText(data), nil
+	}
+
+	return morse.ToMorse(data), nil
+}
+
+func isMorse(data string) bool {
+	for _, r := range data {
+		if r != '.' && r != '-' && r != ' ' && r != '\n' && r != '\r' && r != '\t' {
+			return false
+		}
+	}
+	return true
+}
