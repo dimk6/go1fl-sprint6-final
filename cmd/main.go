40cc60de42b475c3427b858bc6cc63dc4af6914e
dimk6 <dimon978@bk.ru>
2026-06-08 00:34:34 +0300

    fix main server start

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "", log.LstdFlags)
+
+	srv := server.NewServer(logger)
+
+	logger.Println("server started at http://localhost:8080")
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		logger.Fatal(err)
+	}
 }
